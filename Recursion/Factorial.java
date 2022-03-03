627be40e845642825210ef91736e2b049b7a7231
Sahilbalhra <sahilbalhra0@gmail.com>
2022-03-04 02:12:48 +0530

    factorial and power question added

---
+++ Recursion/Factorial.java
@@ -0,0 +1,29 @@
+package Recursion;
+// 1. You are given a number n.
+// 2. You are required to calculate the factorial of the number. Don't change the signature of factorial function.
+// Example
+// Sample Input
+// 5
+// Sample Output
+// 120
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static int solution(int n){
+        if(n==0||n==1){
+            return 1;
+        }
+       int fn=solution(n-1);
+       return n*fn;
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n =scn.nextInt();
+        int ans=solution(n);
+        System.out.println(ans);
+        scn.close();
+        
+    }
+    
+}
---
+++ Recursion/Powerlinear.java
@@ -0,0 +1,32 @@
+package Recursion;
+// 1. You are given a number x.
+// 2. You are given another number n.
+// 3. You are required to calculate x raised to the power n. Don't change the signature of power function .
+// Sample Input
+// 2
+// 5
+// Sample Output
+// 32
+
+import java.util.Scanner;
+
+public class Powerlinear {
+    public static int power(int n,int p){
+        if(p==0){
+            return 1;
+        }
+        int rec=power(n, p-1);
+        int ans =n*rec;
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int num=scn.nextInt();
+        int pow=scn.nextInt();
+        int myans=power(num,pow);
+        System.out.println(myans);
+        scn.close();
+        
+    }
+    
+}
