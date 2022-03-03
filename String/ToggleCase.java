edbd5fbed816b3f5cb97651cb33078251927ec6c
Sahilbalhra <sahilbalhra0@gmail.com>
2022-03-03 23:43:55 +0530

    ToggleCase Question Added

---
+++ String/ToggleCase.java
@@ -0,0 +1,35 @@
+package String;
+// 1. You are given a string that contains only lowercase and uppercase alphabets. 
+// 2. You have to toggle the case of every character of the given string.
+// Sample Input
+// pepCODinG
+// Sample Output
+// PEPcodINg
+
+import java.util.Scanner;
+
+public class ToggleCase {
+    public static void solution(String str){
+        String s="";
+        for(int i=0; i<str.length();i++){
+         char ch=str.charAt(i);
+         if(ch>='A' && ch<='Z'){
+             //uper to lower
+             char lw=(char)(ch-'A'+'a');
+             s+=lw;
+         }else{
+             //lower to uper
+             char up=(char)(ch-'a'+'A');
+             s+=up;
+         }
+        }
+        System.out.println(s);
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str=scn.nextLine();
+        solution(str);
+        
+    }
+    
+}
