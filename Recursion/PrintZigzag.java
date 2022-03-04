2fbf9341d211dfefbd0a829af6af98d04b12c694
Sahilbalhra <sahilbalhra0@gmail.com>
2022-03-04 20:23:11 +0530

    ZigZag and tower of Hanoi added

---
+++ Recursion/PrintZigzag.java
@@ -0,0 +1,35 @@
+package Recursion;
+import java.util.Scanner;
+// 1. Here are a few sets of inputs and outputs for your reference
+// Input1 -> 1
+// Output1 -> 1 1 1
+
+// Input2 -> 2
+// Output2 -> 2 1 1 1 2 1 1 1 2
+
+// Input2 -> 3
+// Output3 -> 3 2 1 1 1 2 1 1 1 2 3 2 1 1 1 2 1 1 1 2 3
+
+// 2. Figure out the pattern and complete the recursive function pzz to achieve the above for any positive number n.
+
+public class PrintZigzag {
+    public static void solution(int n){
+        if(n==0){
+            return;
+        }
+        System.out.print(n+ " ");
+        solution(n-1);
+        System.out.print(n+ " ");
+        solution(n-1);
+        System.out.print(n+ " ");
+
+    }
+    public static void main(String[] args) {
+        Scanner scn =new Scanner(System.in);
+        int n=scn.nextInt();
+        solution(n);
+       
+        scn.close();
+    }
+    
+}
---
+++ Recursion/TowerOfHanoi.java
@@ -0,0 +1,49 @@
+package Recursion;
+
+import java.util.Scanner;
+
+// 1. There are 3 towers. Tower 1 has n disks, where n is a positive number. Tower 2 and 3 are empty. 
+// 2. The disks are increasingly placed in terms of size such that the smallest disk is on top and largest disk is at bottom.
+// 3. You are required to
+//  3.1. Print the instructions to move the disks. 
+//  3.2. from tower 1 to tower 2 using tower 
+//  3 3.3. following the rules
+//    3.3.1 move 1 disk at a time.
+//    3.3.2 never place a smaller disk under a larger disk.
+//    3.3.3 you can only move a disk at the top.
+// Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is.Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+
+// Sample Input
+// 3
+// 10
+// 11
+// 12
+// Sample Output
+// 1[10 -> 11]
+// 2[10 -> 12]
+// 1[11 -> 12]
+// 3[10 -> 11]
+// 1[12 -> 10]
+// 2[12 -> 11]
+// 1[10 -> 11]
+
+public class TowerOfHanoi {
+    public static void toh(int n,int t1 ,int t2, int t3){
+        if(n==0){
+            return;
+        }
+        toh(n-1,t1,t3,t2);
+        System.out.println(n+"["+t1+"->"+t2+"]");
+        toh(n-1,t3,t2,t1);
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n =scn.nextInt();
+        int t1 =scn.nextInt();
+        int t2 =scn.nextInt();
+        int t3 =scn.nextInt();
+        toh(n,t1,t2,t3);
+        scn.close();
+    }
+    
+}
