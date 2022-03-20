e847ed0bae1b08fabe71c1dae8c714225af95d46
Sahilbalhra <sahilbalhra0@gmail.com>
2022-03-20 09:31:03 +0530

    Number pattern is added

---
+++ Pattern/NumberPattern.java
@@ -0,0 +1,26 @@
+package Pattern;
+
+import java.util.*;
+
+public class NumberPattern {
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        int num = scn.nextInt();
+        int ans = 0;
+        String str = "";
+        for (int i = 1; i <= num; i++) {
+
+            ans += i;
+            if (i == 1) {
+                str += i;
+                System.out.println(str + " = " + ans);
+            } else {
+                str += " + "+i;
+                System.out.println(str + " = " + ans);
+            }
+        }
+        scn.close();
+    }
+
+}
