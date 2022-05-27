c9c84de2fb7a6c42504d70a57bb057097f0e5ff5
Sahilbalhra <sahilbalhra0@gmail.com>
2022-05-27 12:45:24 +0530

    genric tree started

---
+++ Tree1&2/Genric Tree/GenricTreeConstructor.java
@@ -0,0 +1,5 @@
+package Tree1&2.Genric Tree;
+
+public class GenricTreeConstructor {
+    
+}
---
+++ simple java program/codeChef.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class codeChef {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        ArrayList<Integer> arr=new ArrayList<>();
+        for(int i=0;i<=n;i++){
+            if(i==n){
+                int num=2;
+                for(int j=1;j<n;j++){
+                    
+                   num*=num;
+                }
+
+            }
+        }
+    }
+    
+}
